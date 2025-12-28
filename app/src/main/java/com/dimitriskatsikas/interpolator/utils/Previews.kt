2b0fb552e74cfb7230e67a26c7fc4837a2c2b2a4
d.katsikas <d.katsikas@withplum.com>
2025-12-28 22:22:39 +0200

    chore(preview): Show system UI in all Compose previews
    This commit updates the custom `@Previews` annotation to enable the system UI in all screenshot previews.
    
    - **`utils/Previews.kt`**:
        - The `showSystemUi = true` parameter has been added to all `@Preview` annotations within the custom `@Previews` definition. This ensures that UI previews for different devices, font scales, and color modes will now render with the system status and navigation bars visible, providing a more realistic context.

--- app/src/main/java/com/dimitriskatsikas/interpolator/utils/Previews.kt
+++ app/src/main/java/com/dimitriskatsikas/interpolator/utils/Previews.kt
@@ -10,22 +10,26 @@ import androidx.compose.ui.tooling.preview.Preview
     name = "Default | x1 font | Light mode",
     device = Devices.DEFAULT,
     fontScale = 1f,
+    showSystemUi = true,
     uiMode = Configuration.UI_MODE_NIGHT_NO
 )
 @Preview(
     name = "Default | x1 font | Dark mode",
     device = Devices.DEFAULT,
     fontScale = 1f,
-    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL
+    showSystemUi = true,
+    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL,
 )
 @Preview(
     name = "Small | x2 font",
     device = Devices.PIXEL_2,
-    fontScale = 2f
+    fontScale = 2f,
+    showSystemUi = true
 )
 @Preview(
     name = "Tablet | x1 font",
     device = "spec:width=800dp,height=1280dp,dpi=240",
     fontScale = 1f,
+    showSystemUi = true
 )
 annotation class Previews
